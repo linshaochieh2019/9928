fc3f27cc518f79289c862cdc2e5a653e543d5038
Jay <linshaochieh2019@gmail.com>
2025-09-05 17:18:37 +0800

    Started teacher/list work

--- client/src/app/components/teacher/list/list.ts
+++ client/src/app/components/teacher/list/list.ts
@@ -9,12 +9,15 @@ import { Teacher } from '../../../models/teacher.model';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './list.html',
+  styleUrls: ['./list.scss'],
 })
 export class TeacherListComponent implements OnInit {
   teachers: Teacher[] = [];
   loading = true;
+  page = 1;               // current page
+  pageSize = 12;          // teachers per page
 
-  constructor(private teacherService: TeacherService) {}
+  constructor(private teacherService: TeacherService) { }
 
   ngOnInit(): void {
     this.teacherService.getTeachers().subscribe({
@@ -27,5 +30,30 @@ export class TeacherListComponent implements OnInit {
         this.loading = false;
       },
     });
+  }
+
+  contactTeacher(teacher: any) {
+    console.log('Contact teacher clicked:', teacher);
+    // TODO: open modal / redirect to login / trigger contact flow
+  }
+
+  get paginatedTeachers() {
+    const start = (this.page - 1) * this.pageSize;
+    return this.teachers.slice(start, start + this.pageSize);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.teachers.length / this.pageSize);
+  }
+
+  // Generate array [1, 2, ..., totalPages]
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(newPage: number) {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.page = newPage;
+    }
   }
 }
